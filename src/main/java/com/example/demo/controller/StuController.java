75679f55fed0c22de17723c7d43035637bfd0804
zhouwenquan <1471754591@qq.com>
2020-09-21 16:14:03 +0800

    first commit

--- src/main/java/com/example/demo/controller/StuController.java
+++ src/main/java/com/example/demo/controller/StuController.java
@@ -43,7 +43,7 @@ public class StuController {
 
     @GetMapping("/get2")
     public String f3(){
-        return "HELLO ACTIONS";
+        return "HELLO ACTIONS1";
     }
 
     @GetMapping(value = "/update")
